f16fc99e4ed3e74ec3f3c72cd9935ac56a1f4c4a
Lucas Nery <lucas.leite1@br.nestle.com>
2024-12-11 11:27:58 -0300

    remove models

--- src/models/card.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const cardSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  username: { type: String, required: true },
-  color: { type: String, required: true, },
-});
-
-module.exports = mongoose.model('Card', cardSchema);
--- src/models/person.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-const personSchema = new mongoose.Schema({
-	name: { type: String, required: true },
-	username: { type: String, required: true }
-})
-
-module.exports = mongoose.model('Person', personSchema);
