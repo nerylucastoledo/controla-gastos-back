2b5b1a5d3e9f703b2e3adf557217aa250bcea4dc
Lucas Nery <lucasnerytoledo@Hotmail.com>
2025-02-18 09:35:12 -0300

    improvement cookies

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -24,6 +24,13 @@ const authMiddleware = async (req: Request, res: Response, next: NextFunction) =
     req.user = decodedToken;
     next();
   } catch (error) {
+    console.error(error);
+    res.clearCookie('access_token', {
+      path: '/',
+      httpOnly: true,
+      sameSite: 'none',
+      secure: true
+    });
     return res.status(401).send({ message: 'Token inválido ou expirado.', error: 403, ok: false });
   }
 };
