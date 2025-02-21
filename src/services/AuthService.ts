26b332ca041ad52947172e04c512a7c657e1a35b
Lucas Nery <lucasnerytoledo@hotmail.com>
2025-02-21 13:38:11 -0300

    fix token

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -61,7 +61,7 @@ class AuthService {
   public async token(): Promise<{ token: any }> {
     try {
       const token = Firebase.getToken();
-      const decodedToken = await Firebase.getAdminInstance().auth().verifyIdToken(token + "a");
+      const decodedToken = await Firebase.getAdminInstance().auth().verifyIdToken(token);
       return { token: decodedToken };
     } catch (error) {
       throw new Error((error as Error).message || 'Ocorreu um erro interno');
